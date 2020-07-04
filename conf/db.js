21ebcbe25a29f6192da4e97a4d3a14540e34e88f
huangguoqiang <kookokukyo@gmail.com>
2020-07-04 11:59:06 +0800

    '添加mysql数据库连接和接口配置'

--- conf/db.js
+++ conf/db.js
@@ -1,9 +1,9 @@
 module.exports = {
     mysql: {
-        host: '127.0.0.1',
+        host: '81.68.116.117',
         user: 'root',
         password: '84825877Hh!',
-        database: 'muzi_data', // 前面建的user表位于这个数据库中
+        database: 'MuziData', // 前面建的user表位于这个数据库中
         port: 3306
     }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/',function(req,res)  {
+    res.send({
+        code: 200,
+        status: "success",
+        message: "APP启动成功！",
+        data: []
+    });
+});
 
 module.exports = router;
